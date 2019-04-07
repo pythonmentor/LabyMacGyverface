f1401c05d6b36d568abc19ea7aff458cd3bd996e
Thierry Chappuis <thierry@chappuis.io>
2019-04-07 08:28:49 +0200

    Commentaires sur le jeu. Excellent travail

--- labyrinthe.py
+++ labyrinthe.py
@@ -1,6 +1,21 @@
 #! /usr/bin/env python3
 # coding: utf-8
 
+# -tc- Sur le plan du jeu, il est fonctionnel. Il manque un compteur d'objets
+# -tc- qui affiche les objets ramassés. Mais excellent job!
+
+# -tc- Sur le plan de la PEP8, flake8 retourne une vingtaine de remarques qu'il
+# -tc- devrait être facile à corriger.
+
+# -tc- Le code est globalement de bonne qualité et bien documenté. Il y a des
+# -tc- complications inutiles qui offrent quelques perspectives d'amélioration,
+# -tc- mais il est clairement à niveau pour une soutenance.
+
+# -tc- TODO: séparer chaque classe dans son module pour entraîner les 
+# -tc- import, créer un Pipfile avec pipenv (pip install pipenv) pour gérer les
+# -tc- dépendances et rédiger un README.md qui décrit le processus 
+# -tc- d'installation et de lancement du jeu.
+
 
 '''This is the main module. It contains the main function
 that enables gameplay loop. It also initializes display window.
@@ -10,11 +25,17 @@ that enables gameplay loop. It also initializes display window.
 import json
 import random
 import pygame
+# -tc- de manière générale, éviter l'usage de l'* dans les import qui est
+# proscrite par la PEP8
 from pygame.locals import *
 
 
-
+# -tc- Le pygame.init() peut être fait au début du main()
 pygame.init() #enables sprite initialization during Display class declaration
+
+# -tc- L'usage de variables globales n'est pas une bonne habitude et on peut
+# -tc- facilement l'éviter à l'aide de pygame.display.get_surface(). Définir
+# -tc- window au début du main.
 WINDOW = pygame.display.set_mode((750, 750))
 
 class Macgyver:
@@ -51,6 +72,7 @@ class Macgyver:
             else:
                 Display.STATE = "Defeat"
                 
+            # -tc- Pourquoi Mac fait-il un flip()?
             pygame.display.flip()
 
         elif new_square.content in (2, 3, 4):
@@ -63,10 +85,17 @@ class Macgyver:
             3: "pipe",
             4: "ether"
         }
+        # -tc- Donc il est possible d'ajouter 3 "invalid content_id" à la liste
+        # -tc self.items et de gagner?
         self.items.append(switcher.get(content_id, "invalid content_id"))
 
 
     ### MOVEMENT ###
+
+    # -tc- inutilement complexe, mais fonctionnel. Beaucoup de nombres magiques
+    # -tc- utilisés en dur: 14, 15, 5. Beaucoup de répétition de code. En
+    # -tc- choisissant bien la structure de données, il est possible de tout 
+    # -tc- réduire à 3 lignes.
 
     def move_right(self):
         '''Checks grid borders using square logic, then check walls
@@ -76,6 +105,9 @@ class Macgyver:
             if GameController.SQUARE_ARRAY[next_position].content != 5:
                 self.position = next_position
 
+    # -tc- same as above ne fait pas de sens dans un contexte d'appel à la 
+    # -tc- fonction help(). Ecrire les docstrings de manière indépendante les 
+    # une des autres
     def move_up(self):
         '''Same as above'''
         if self.current_square.y_pos > 0:
@@ -97,6 +129,8 @@ class Macgyver:
             if GameController.SQUARE_ARRAY[next_position].content != 5:
                 self.position = next_position
 
+# -tc ajouer un espace après la doctring et ajouter une docstring à la méthode 
+# -tc- d'initialisation
 class Square:
     '''Square object is used to store coordinates and id
     Attributes are called to locate items and agents on the grid'''
@@ -121,6 +155,8 @@ class GameController:
         '''Generates fresh empty grid and add items on it using the method below'''
         cls.SQUARE_ARRAY = []
 
+        # -tc- Ici, tu suppose que la taille du labyrinthe ne pourra pas
+        # -tc- changer. Mieux vaut déduire la taille du fichier json.
         for i in range(0, 15):
             for j in range(0, 15):
 
@@ -129,12 +165,22 @@ class GameController:
 
         cls.populate_squares()
 
+    # -tc- le format de wall-layout.json est original. Pas si facile à 
+    # -tc- à maintenir pour un labyrinthe contenant plus que quelques murs (ca
+    # -tc- serait bien pour la soutenance). Eventuellement, un générateur 
+    # -tc- pourrait en faciliter la création.
 
     @classmethod
     def populate_squares(cls):
         '''Creates items on squares. Nature of item is defined by an int
         that is stored in the content attribute of the square :
         1 is warden, 2 to 4 are pick-ups, 5 is wall, 6 is character'''
+
+        # -tc- spécifier les passages et non les murs dans ton fichier rendrait
+        # -tc- le code code immédiatement plus court. Il serait alors possible 
+        # -tc- de tirer immédiatement au sort 3 éléments avec random.sample().
+        # -tc- la gestion des méthodes move pourrait également être 
+        # -tc- drastiquement simplifiée.
         for counter, id_key in enumerate(
                 json.load(open("ressources/wall-layout.json"))):
 
@@ -146,6 +192,9 @@ class GameController:
             else:
                 cls.SQUARE_ARRAY[id_key].content = 5
 
+        # -tc- Simplification (encore plus simple si wall-layout est passage-layout)
+        # mac, enc, walls = json.load(open("ressources/wall-layout.json"))
+        # random_ids = random.sample(set(range(225))-set(walls), k=3)
         for i in range(2, 5):
             random_id = random.randrange(0, 225)
 
@@ -156,6 +205,8 @@ class GameController:
             cls.SQUARE_ARRAY[random_id].content = i
 
 
+# -tc- Utiliser des classes fille de pygame.sprite.Sprite peut simplifier la 
+# -tc- gestion. Je séparerais de background des autres objets.
 class Display:
     '''handles sprites loading from ressources folder
     also displays them in the window, based on their position on the grid'''
@@ -208,6 +259,8 @@ class Display:
                 elif square.content == 4:
                     WINDOW.blit(cls.ETHER_SPRITE, (x_pix_pos, y_pix_pos))
 
+                # -tc- pas nécessaire de bliter les murs à chaque fois
+
                 elif square.content == 5:
                     WINDOW.blit(cls.WALL_SPRITE, (x_pix_pos, y_pix_pos))
 
@@ -249,6 +302,7 @@ def main():
 
                 character.touch_square()
                 character.current_square.content = 6
+                # -tc- toute l'interface ne change pas à chaque fois
                 Display.refresh_display()
 
     while GameController.GAME_OVER:
